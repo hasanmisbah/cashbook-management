2108706684059254ae4c852320668ddbafa4f642
Hasan Misbah <hasanmisbah01@gmail.com>
2021-11-21 18:40:18 +0600

    Vue 3 Added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::any('{any?}/{any1?}/{any2?}/{any3?}', fn() => view("welcome"))->name('home');
